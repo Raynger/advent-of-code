6acd7b043c47c724b2ec60bea674398f8b5fa4fe
Ray Chen <raychenwk@google.com>
2021-12-11 02:07:21 +1100

    Day 7

---
+++ day7/B.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func f(A []int, n int) int {
+	total := 0
+	for _, v := range A {
+		diff := int(math.Abs(float64(v - n)))
+		total += (diff * (diff + 1)) / 2
+	}
+	return total
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+
+	var A []int
+
+	sc.Scan()
+	s := strings.Split(sc.Text(), ",")
+
+	min := 0x3f3f3f3f
+	max := -1
+	for i := range s {
+		n, _ := strconv.Atoi(s[i])
+		A = append(A, n)
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+	}
+
+	for min != max {
+		mid := (min + max) / 2
+		if f(A, mid) < f(A, mid+1) {
+			max = mid
+		} else {
+			min = mid + 1
+		}
+	}
+	fmt.Println(f(A, min))
+}
